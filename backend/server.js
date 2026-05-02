da084d09f6be985c410a357cd800e97145d7d9a2
Prince2202031800047 <pckavathiya1@gmail.com>
2026-05-02 14:59:53 +0530

    fix frontend path final

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,11 @@ app.get("/health", (req, res) => {
 // =======================
 // 🔥 FRONTEND SERVE (FIXED)
 // =======================
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
 // =======================
